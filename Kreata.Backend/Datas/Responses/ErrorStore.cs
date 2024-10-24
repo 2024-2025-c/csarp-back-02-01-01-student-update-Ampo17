af7b31e5851756f5db58a154c2cc5638189eab48
c9ampann <c9ampann@vasvari.org>
2024-10-24 10:20:09 +0200

    error

---
+++ Kreata.Backend/Datas/Responses/ErrorStore.cs
@@ -0,0 +1,22 @@
+﻿namespace Kreata.Backend.Datas.Responses
+{
+    public class ErrorStore
+    {
+        public ErrorStore(string error)
+        {
+            Error = string.Empty;
+        }
+
+        public string Error { get; set; } = string.Empty;
+        public bool HasError => !string.IsNullOrEmpty(Error);
+        public void ClearAndAddError(String error)
+        {
+            Error = error;
+        }
+        public void AppendNewError(string error)
+        {
+            Error = $"{Error}\n{error}";
+        }
+
+    }
+}
